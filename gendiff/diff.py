7f1d0182e2a520caf1e91d3a2b6b987a57ec3508
Slava Kalnichenko <slavaslayc@gmail.com>
2024-04-28 20:38:15 +0300

    Remove comment

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -1,16 +1,10 @@
 from gendiff.comparator import compare_data
-from gendiff.stylish import stylish
+from gendiff.formatters.format_selection import choose_formatter
 from gendiff.parser import parser
 
 
-def generate_diff(file_path1, file_path2, formatter='stylish'):
+def generate_diff(file_path1, file_path2, format_name='stylish'):
     data1 = parser(file_path1)
     data2 = parser(file_path2)
     raw_data = compare_data(data1, data2)
-    return stylish(raw_data)
-
-
-# file1_path = '../tests/fixtures/file_nested1.yaml'
-# file2_path = '../tests/fixtures/file_nested2.yaml'
-# diff = generate_diff(file1_path, file2_path)
-# print(diff)
+    return choose_formatter(raw_data, format_name)
