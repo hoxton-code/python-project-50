4098b98844105702203fb56cbe75a4e475b5d1a1
Slava Kalnichenko <slavaslayc@gmail.com>
2024-04-28 20:42:16 +0300

    Add json format output feature and tests

---
+++ gendiff/formatters/json.py
@@ -0,0 +1,5 @@
+import json
+
+
+def format_json(data):
+    return json.dumps(data, indent=4)
--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -2,17 +2,43 @@ from gendiff.diff import generate_diff
 import pytest
 
 
-@pytest.mark.parametrize("file_path_1, file_path_2, expected_output", [
-    ("tests/fixtures/file1.json", "tests/fixtures/file2.json",
-     "tests/fixtures/expected_plain_output.txt"),
-    ("tests/fixtures/file1.yaml", "tests/fixtures/file2.yaml",
-     "tests/fixtures/expected_plain_output.txt"),
-    ("tests/fixtures/file_nested1.json", "tests/fixtures/file_nested2.json",
-     "tests/fixtures/expected_nested_output.txt"),
-    ("tests/fixtures/file_nested1.yaml", "tests/fixtures/file_nested2.yaml",
-     "tests/fixtures/expected_nested_output.txt"),
-])
-def test_generate_flat_diff(file_path_1, file_path_2, expected_output):
+@pytest.mark.parametrize("file_path_1, file_path_2, format_name, "
+                         "expected_output", [
+                             ("tests/fixtures/file1.json",
+                              "tests/fixtures/file2.json",
+                              'stylish',
+                              "tests/fixtures/expected_flat_output.txt"),
+                             ("tests/fixtures/file1.yaml",
+                              "tests/fixtures/file2.yaml",
+                              'stylish',
+                              "tests/fixtures/expected_flat_output.txt"),
+                             ("tests/fixtures/file_nested1.json",
+                              "tests/fixtures/file_nested2.json",
+                              'stylish',
+                              "tests/fixtures/expected_nested_output.txt"),
+                             ("tests/fixtures/file_nested1.yaml",
+                              "tests/fixtures/file_nested2.yaml",
+                              'stylish',
+                              "tests/fixtures/expected_nested_output.txt"),
+                             ("tests/fixtures/file_nested1.json",
+                              "tests/fixtures/file_nested2.json",
+                              'plain',
+                              "tests/fixtures/expected_plain_output.txt"),
+                             ("tests/fixtures/file_nested1.yaml",
+                              "tests/fixtures/file_nested2.yaml",
+                              'plain',
+                              "tests/fixtures/expected_plain_output.txt"),
+                             ("tests/fixtures/file_nested1.yaml",
+                              "tests/fixtures/file_nested2.yaml",
+                              'json',
+                              "tests/fixtures/expected_json_output.txt"),
+                             ("tests/fixtures/file_nested1.json",
+                              "tests/fixtures/file_nested2.json",
+                              'json',
+                              "tests/fixtures/expected_json_output.txt"),
+                         ])
+def test_generate_flat_diff(file_path_1, file_path_2, format_name,
+                            expected_output):
     with open(expected_output) as file:
         exp = file.read()
-    assert generate_diff(file_path_1, file_path_2) == exp
+    assert generate_diff(file_path_1, file_path_2, format_name) == exp
